404114de76176e64dc3fea139102cb6604f5a5c7
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-08-06 21:07:12 +0300

    stopped the default config from overwriting the user's custom configs' '

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from cli.cli import Cli
+from util.config_obj import Config
 
 def main():
     cli = Cli()
     args = cli.setup()
+    config = Config()
+    config_file = config.create_config_file()
+    config.generate_default_config(file_path=config_file)
 
 
 if __name__ == "__main__":
